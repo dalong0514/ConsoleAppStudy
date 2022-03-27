2c963023b8279d9cfc917953d784c17e6c183e3d
dalong <dalong0514@me.com>
2022-03-27 10:50:54 +0800

    初始化项目

---
+++ ConsoleAppNetFramework/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppNetFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12 * 30;
+            Console.WriteLine(x);
+        }
+    }
+}

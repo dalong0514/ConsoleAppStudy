c2ae7fc9d30ca266f30c780d692c07ad2846801e
dalong <dalong0514@me.com>
2022-04-03 09:56:52 +0800

    增加专属的学习类

--- ConsoleAppNetFramework/Program.cs
+++ ConsoleAppNetFramework/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using StudyClass;
 
 namespace ConsoleAppNetFramework
 {
@@ -9,8 +10,16 @@ namespace ConsoleAppNetFramework
     {
         static void Main(string[] args)
         {
-            int x = 12 * 30;
-            Console.WriteLine(x);
+            //Console.WriteLine(FeetToInches(30));
+            //Console.WriteLine(FeetToInches(100));
+
+            UnitConverter feetToInchesConverter = new UnitConverter(12);
+            UnitConverter milesToFeetConverter = new UnitConverter(5280);
+            Console.WriteLine(feetToInchesConverter.Convert(30));
+            Console.WriteLine(feetToInchesConverter.Convert(100));
+            Console.WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1)));
+            //Console.WriteLine();
         }
     }
+
 }
---
+++ StudyClass/UnitConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudyClass
+{
+    public class UnitConverter
+    {
+        // Field 
+        int ratio;
+        // Constructor 
+        public UnitConverter(int unitRatio)
+        {
+            ratio = unitRatio;
+        }
+        // Method
+        public int Convert(int unit)
+        {
+            return unit * ratio;
+        }
+        static int FeetToInches(int feet)
+        {
+            int inches = feet * 12;
+            return inches;
+        }
+    }
+}
